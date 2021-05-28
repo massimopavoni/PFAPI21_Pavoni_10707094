5b20416232bb9cb8ee8677157c1c09403ab271bc
Massimo Pavoni <maspavoni@gmail.com>
2021-05-29 00:44:22 +0200

    Basic program structure
    (missing many things, but a few bones of the skeleton are there)

--- open.c
+++ open.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
+#include <string.h>
+
+// Function declaration
+void add_graph();
+void get_best_graphs();
+
+// Statis constants for string commands
+static const char *ADD_GRAPH_COMMAND = "AggiungiGrafo";
+static const char *GET_BEST_GRAPHS_COMMAND = "TopK";
 
 int main()
 {
-  int d = 0;
-  scanf("%d", &d);
-  printf("%d", d);
+  // Variable declaration
+  //  - number of graph nodes: int
+  //  - number of best graphs to track: int
+  //  - string command: char*
+  int nodes, best_graphs;
+  char *command;
+
+  // Read graph rank info
+  scanf("%d, %d", &nodes, &best_graphs);
+
+  // Main cycle
+  do
+  {
+    // Read command
+    scanf("%s", &command);
+
+    // Parse command
+    if (strcmp(command, ADD_GRAPH_COMMAND))
+    {
+    }
+    else if (strcmp(command, GET_BEST_GRAPHS_COMMAND))
+    {
+    }
+  } while (strcmp(command, "") != 0); // If EOF, exit cycle
+
+  // The end!
   return 0;
 }
