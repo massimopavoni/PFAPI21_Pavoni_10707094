8a45e545248fae60c3bf82df46e9416a18c56691
Massimo Pavoni <maspavoni@gmail.com>
2021-06-01 19:40:18 +0200

    TopK with singly linked list for best graphs, Not Working
    - With top k behaviour, implemented all features of the  final program, nothing works lol
    - TODO: fix Dijkstra with min-heap and sum, find why linked list prints more than it should

--- open.c
+++ open.c
@@ -20,25 +20,54 @@ typedef struct
 // MinHeap struct
 typedef struct
 {
-  // Array of keys: VDTuple *
-  VDTuple *keys;
-
   // Current position in the array: unsigned int
   unsigned int heapSize;
 
   // Maximum size of the heap: unsigned int
   unsigned int size;
 
+  // Array of keys: VDTuple *
+  VDTuple *keys;
+
 } MinHeap;
+
+// GFElement struct
+typedef struct s_GFElement GFElement;
+
+struct s_GFElement
+{
+  // Graph index: unsigned int
+  unsigned int graphIndex;
+
+  // Graph fitness: unsigned int *
+  unsigned int *graphFitness;
+
+  // Next list element: GFElement *
+  GFElement *next;
+};
+
+// List struct
+typedef struct
+{
+  // List length: unsigned int
+  unsigned int length;
+
+  // Maximum list length: unsigned int *
+  unsigned int *maxLength;
+
+  // First element: GFElement *
+  GFElement *first;
+
+} List;
 
 // Functions declaration
 
-// Add graph to read edges info and get sum of shortest paths.
+// Get graph fitness after reading edges info.
 //
 // Return type: unsigned int *
 // Arguments
 // - number of vertices variable pointer: unsigned int*
-unsigned int *AddGraph(unsigned int *vertices);
+unsigned int *GetGraphFitness(unsigned int *vertices);
 
 // Dijkstra's algorithm to calculate shortest paths from source to all other vertices and get the total sum.
 //
@@ -90,7 +119,36 @@ VDTuple *MinHeapPopMin(MinHeap *minHeap);
 // - root index from which the function starts: unsigned int
 void MinHeapify(MinHeap *minHeap, unsigned int root);
 
-void GetBestGraphs();
+// GFElement init function to allocate memory for the element and return pointer.
+//
+// Return type: GFElement *
+// Arguments
+// - graph index since the start of the program: unsigned int
+// - graph fitness value obtained through shortest paths sum: unsigned int *
+GFElement *GFElementInit(unsigned int graphIndex, unsigned int *graphFitness);
+
+// List init function to allocate memory for the list and return pointer.
+//
+// Return type: List *
+// Arguments
+// - max length of the list: unsigned int *
+List *ListInit(unsigned int *maxLength);
+
+// List insert function to check if graph goes in the ranking and pop worst graph if needed.
+//
+// Return type: void
+// Arguments
+// - best graphs list structure pointer: List *
+// - graph index since the start of the program: unsigned int
+// - graph fitness value obtained through shortest paths sum: unsigned int *
+void ListInsertGraph(List *bestGraphsList, unsigned int graphIndex, unsigned int *graphFitness);
+
+// List print function to get the best graphs.
+//
+// Return type: void
+// Arguments
+// - best graphs list structure pointer: List *
+void ListPrint(List *bestGraphsList);
 
 // Static constants
 
@@ -119,35 +177,51 @@ int main()
   unsigned int vertices;
 
   // Number of best graphs to track: unsigned int
-  unsigned int best_graphs;
+  unsigned int bestGraphs;
+
+  // Current graph index: unsigned int
+  unsigned int graphIndex = UINT32_MAX;
+
+  // Read graph rank info
+  scanf("%u %u", &vertices, &bestGraphs);
+
+  // Best graphs list: List *
+  List *bestGraphsList = ListInit(&bestGraphs);
 
   // String command: char*
   char *command = malloc(MAX_COMMAND_SIZE * sizeof *command);
 
-  // Read graph rank info
-  scanf("%u %u", &vertices, &best_graphs);
-
   // Main loop
-  while (scanf("%s", command) == 1) // Read command; if EOF, exit loop
+  while (1)
   {
+    // Read command
+    scanf("%s", command);
+
+    // After failed read, if eof exit the loop
+    if (feof(stdin))
+      break;
+
     // Parse command
     if (strcmp(command, ADD_GRAPH_COMMAND) == 0)
     {
-      AddGraph(&vertices);
+      // Increment graph index and insert graph in list (if it should be there)
+      graphIndex++;
+      ListInsertGraph(bestGraphsList, graphIndex, GetGraphFitness(&vertices));
     }
     else if (strcmp(command, GET_BEST_GRAPHS_COMMAND) == 0)
-    {
-    }
+      // Print best graphs
+      ListPrint(bestGraphsList);
   }
 
   // Free memory galore
+  free(bestGraphsList);
   free(command);
 
   // -------- PROFILING --------
 
   clock_t end = clock();
   double elapsed_time = (end - start) / (double)CLOCKS_PER_SEC;
-  fprintf(stderr, "%lf", elapsed_time);
+  fprintf(stderr, "\n%lf", elapsed_time);
 
   // -------- PROFILING --------
 
@@ -159,9 +233,9 @@ int main()
 
 // ------------------------------------------------------------------- Functions implementation ------------------------------------------------------------------
 
-// AddGraph implementation
+// GetGraphFitness implementation
 
-unsigned int *AddGraph(unsigned int *vertices)
+unsigned int *GetGraphFitness(unsigned int *vertices)
 {
   // Allocate adjacency matrix as a single array, usage: *(adjacencyMatrix + row * *vertices + column)
   unsigned int *adjacencyMatrix = malloc(*vertices * *vertices * sizeof *adjacencyMatrix);
@@ -236,13 +310,11 @@ unsigned int *DijkstraSum(unsigned int *vertices, unsigned int *adjacencyMatrix,
       }
     }
 
-    // Add distance to total or exit (graph is not connected)
+    // Add distance to total
     if (*distancesSum == UINT32_MAX)
       *distancesSum = distances[vdTuple->vertex];
     else if (distances[vdTuple->vertex] != UINT32_MAX)
       (*distancesSum) += distances[vdTuple->vertex];
-    else
-      return distances + vdTuple->vertex;
   }
 
   // Free 'em all
@@ -258,7 +330,7 @@ unsigned int *DijkstraSum(unsigned int *vertices, unsigned int *adjacencyMatrix,
 
 MinHeap *MinHeapInit(unsigned int *size)
 {
-  // Allocate min-heap memory and its array
+  // Allocate min-heap memory and its array, initialize size and heap size
   MinHeap *minHeap = malloc(sizeof *minHeap);
 
   minHeap->size = *size;
@@ -349,6 +421,114 @@ void MinHeapify(MinHeap *minHeap, unsigned int root)
 
     // Climb up
     MinHeapify(minHeap, min);
+  }
+}
+
+// GFElementInit implementation
+
+GFElement *GFElementInit(unsigned int graphIndex, unsigned int *graphFitness)
+{
+  // Allocate element memory, initialize graph index, graph fitness and next
+  GFElement *element = malloc(sizeof *element);
+
+  element->graphIndex = graphIndex;
+  element->graphFitness = graphFitness;
+  element->next = NULL;
+
+  // Return pointer to struct
+  return element;
+}
+
+// ListInit implementation
+
+List *ListInit(unsigned int *maxLength)
+{
+  // Allocate list memory, initialize length, max length and first element
+  List *list = malloc(sizeof *list);
+
+  list->length = 0;
+  list->maxLength = maxLength;
+  list->first = NULL;
+
+  // Return pointer to struct
+  return list;
+}
+
+// ListInsertGraph implementation
+
+void ListInsertGraph(List *bestGraphsList, unsigned int graphIndex, unsigned int *graphFitness)
+{
+  // Well...
+  if (*(bestGraphsList->maxLength) > 0)
+  {
+    // Allocate new graph
+    GFElement *newGraph = GFElementInit(graphIndex, graphFitness);
+
+    // Discard new graph case
+    if (bestGraphsList->first != NULL && *((bestGraphsList->first)->graphFitness) == *(newGraph->graphFitness))
+    {
+      free(newGraph);
+      return;
+
+    } // Empty list or first element case
+    else if (bestGraphsList->first == NULL || *((bestGraphsList->first)->graphFitness) >= *(newGraph->graphFitness))
+    {
+      // Insert it as first element, increase list size
+      newGraph->next = bestGraphsList->first;
+      bestGraphsList->first = newGraph;
+      (bestGraphsList->length)++;
+    }
+    // Non-empty list case
+    else
+    {
+      // Get first element
+      GFElement *currentGraph = bestGraphsList->first;
+
+      // Find correct position
+      while (currentGraph->next != NULL && *((currentGraph->next)->graphFitness) < *(newGraph->graphFitness))
+        currentGraph = currentGraph->next;
+
+      // Discard new graph case
+      if (currentGraph->next != NULL && *((currentGraph->next)->graphFitness) == *(newGraph->graphFitness))
+      {
+        free(newGraph);
+        return;
+      }
+
+      // Insert new graph
+      newGraph->next = currentGraph->next;
+      currentGraph->next = newGraph;
+
+      // Pop worst graph if needed
+      if (bestGraphsList->length == *(bestGraphsList->maxLength))
+      {
+        currentGraph = bestGraphsList->first;
+
+        while ((currentGraph->next)->next != NULL)
+          currentGraph = currentGraph->next;
+
+        free(currentGraph->next);
+
+        currentGraph->next = NULL;
+      }
+      else
+        // Increase list size
+        (bestGraphsList->length)++;
+    }
+  }
+}
+
+// ListPrint implementation
+
+void ListPrint(List *bestGraphsList)
+{
+  GFElement *currentGraph = bestGraphsList->first;
+
+  // Just loop
+  while (currentGraph != NULL)
+  {
+    printf("%u ", currentGraph->graphIndex);
+    currentGraph = currentGraph->next;
   }
 }
 
